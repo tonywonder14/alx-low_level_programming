ad3a29d915658f83aebc58945621b3432c19eddc
tonywonder14 <ekenethony.91@gmail.com>
2023-02-27 14:48:18 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 /**
  * _strlen - Returns the length of a string
  * @str: The string to get the length of
- * Return: The length of @str
+ * Return: The length of @str.
  */
 size_t strlen(const char *str)
 {
